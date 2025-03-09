1e211579c556b4c9dadea78c87076b8de738c22f
Andre Wachsmuth <sensenmann5@gmail.com>
2025-03-10 00:42:43 +0100

    Add sandbox page

---
+++ packages/lecture/src/pages/sandbox/index.tsx
@@ -0,0 +1,178 @@
+import "./layout.css";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primeflex/primeflex.css";
+
+import Layout from "@theme/Layout";
+import { useColorMode, type ColorMode } from "@docusaurus/theme-common";
+
+import useLocalStorage from "react-use-localstorage";
+import { useRef, type ReactNode, useEffect, type RefObject } from "react";
+
+import { Editor as MonacoEditor } from "@monaco-editor/react";
+
+import { PrimeReactProvider } from "primereact/api";
+import { Button } from "primereact/button";
+import { Checkbox } from "primereact/checkbox";
+import { TabView, TabPanel } from "primereact/tabview";
+
+import InitialHtml from "./initial.html";
+const InitialCss = `html {
+  font-family: sans-serif;
+  font-size: 16px;
+}
+
+body {
+  margin: 1em;
+}
+
+h1 {
+  font-size: 1.5em;
+}
+`;
+const InitialJs = "";
+
+import {
+  BooleanSerializer,
+  IntegerSerializer,
+  prepareHtmlContent,
+  useMappedLocalStorage,
+} from "./util";
+
+function applyHtml(
+  ref: RefObject<HTMLIFrameElement | null>,
+  html: string,
+  css: string,
+  js: string,
+  colorMode: ColorMode,
+) {
+  const preparedHtmlContent = prepareHtmlContent(html, css, js, colorMode);
+  const iframe = ref.current;
+  const iframeDoc = iframe?.contentDocument;
+  if (iframe && iframeDoc) {
+    try {
+      iframeDoc.open();
+      iframeDoc.write(preparedHtmlContent);
+      iframeDoc.close();
+    } catch (e) {
+      console.log("Could not write content to iframe", e);
+    }
+  }
+}
+
+function Sandbox(): ReactNode {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  // const outputRef = useRef<HTMLPreElement>(null);
+
+  const { colorMode } = useColorMode();
+
+  const [html, setHtml] = useLocalStorage("sandbox_html", InitialHtml);
+  const [css, setCss] = useLocalStorage("sandbox_css", "");
+  const [js, setJs] = useLocalStorage("sandbox_js", "");
+  const [applyImmediately, setApplyImmediately] = useMappedLocalStorage(
+    "sandbox_immediate",
+    false,
+    BooleanSerializer,
+  );
+  const [activeIndex, setActiveIndex] = useMappedLocalStorage(
+    "sandbox_index",
+    0,
+    IntegerSerializer,
+  );
+
+  const onReset = () => {
+    setHtml(InitialHtml);
+    setCss(InitialCss);
+    setJs(InitialJs);
+  };
+
+  useEffect(() => {
+    if (applyImmediately) {
+      applyHtml(iframeRef, html, css, js, colorMode);
+    }
+  }, [html, css, js, applyImmediately, colorMode]);
+
+  return (
+    <div className="sandbox">
+      <h1>HTML Sandbox</h1>
+      <p>
+        Einfache Sandbox, hier kann man HTML-Schnippsel schreiben und
+        ausprobieren.
+      </p>
+      <div className="sandbox__options">
+        <div className="inline-flex align-items-center">
+          <Checkbox
+            inputId="apply-immediately"
+            checked={applyImmediately}
+            onChange={(e) => setApplyImmediately(e.target.checked ?? false)}
+          />
+          <label className="ml-2" htmlFor="apply-immediately">
+            Änderungen sofort anwenden?
+          </label>
+        </div>
+
+        <Button
+          className="sandbox__button"
+          label="Zurücksetzen"
+          onClick={() => onReset()}
+        />
+        {!applyImmediately && (
+          <Button
+            className="sandbox__button"
+            label="Anwenden"
+            onClick={() => applyHtml(iframeRef, html, css, js, colorMode)}
+          />
+        )}
+      </div>
+      <div className="sandbox__code">
+        <div className="sandbox__code-left">
+          <TabView
+            activeIndex={activeIndex}
+            onTabChange={(e) => setActiveIndex(e.index)}
+          >
+            <TabPanel header="HTML">
+              <MonacoEditor
+                value={html}
+                language="html"
+                options={{ automaticLayout: true }}
+                onChange={(value) => setHtml(value ?? "")}
+              />
+            </TabPanel>
+            <TabPanel header="CSS">
+              <MonacoEditor
+                value={css}
+                language="css"
+                options={{ automaticLayout: true }}
+                onChange={(value) => setCss(value ?? "")}
+              />
+            </TabPanel>
+            <TabPanel header="JavaScript">
+              <MonacoEditor
+                value={js}
+                language="javascript"
+                options={{ automaticLayout: true }}
+                onChange={(value) => setJs(value ?? "")}
+              />
+            </TabPanel>
+          </TabView>
+        </div>
+        <div className="sandbox__code-right">
+          <iframe
+            className="sandbox__iframe"
+            title="HTML-Sandbox"
+            ref={iframeRef}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function SandboxPage(): ReactNode {
+  return (
+    <Layout>
+      <PrimeReactProvider value={{ locale: "en" }}>
+        <Sandbox />
+      </PrimeReactProvider>
+    </Layout>
+  );
+}
---
+++ packages/lecture/src/pages/sandbox/util.ts
@@ -0,0 +1,80 @@
+import type { ColorMode } from "@docusaurus/theme-common";
+import useLocalStorage from "react-use-localstorage";
+
+const parser = new DOMParser();
+
+const basicStyleLight = `
+html {
+  color: black;
+}
+`;
+const basicStyleDark = `
+html {
+  color: white;
+}
+a {
+  color: #9898ff;
+}
+`;
+
+export interface Serializer<T> {
+  serialize: (v: T) => string;
+  deserialize: (v: string) => T;
+}
+
+export function prepareHtmlContent(
+  html: string,
+  css: string,
+  js: string,
+  colorMode: ColorMode,
+): string {
+  const doc = parser.parseFromString(html, "text/html");
+
+  {
+    const styleTheme = document.createElement("style");
+    styleTheme.textContent =
+      colorMode === "dark" ? basicStyleDark : basicStyleLight;
+    doc.head.appendChild(styleTheme);
+  }
+
+  if (css.length > 0) {
+    const styleCustom = document.createElement("style");
+    styleCustom.textContent = css;
+    doc.head.appendChild(styleCustom);
+  }
+
+  if (js.length > 0) {
+    const scriptCustom = document.createElement("script");
+    scriptCustom.textContent = js;
+    doc.body.appendChild(scriptCustom);
+  }
+
+  return doc.documentElement.outerHTML;
+}
+
+export const BooleanSerializer: Serializer<boolean> = {
+  deserialize: (v) => v === "true",
+  serialize: (v) => (v ? "true" : "false"),
+};
+
+export const IntegerSerializer: Serializer<number> = {
+  deserialize: (v) => {
+    const parsed = Number.parseInt(v);
+    return Number.isNaN(parsed) ? 0 : parsed;
+  },
+  serialize: (v) => v.toString(10),
+};
+
+/** Same as useLocalStorage, but lets you use non-string types. */
+export function useMappedLocalStorage<T>(
+  key: string,
+  initial: T,
+  serializer: Serializer<T>,
+): [T, React.Dispatch<T>] {
+  const initialString = serializer.serialize(initial);
+  const [value, setValue] = useLocalStorage(key, initialString);
+  return [
+    serializer.deserialize(value),
+    (v) => setValue(serializer.serialize(v)),
+  ];
+}
