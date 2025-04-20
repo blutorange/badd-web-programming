14cd34d563dc5faea48ca428f5af18c09a86aaa8
Andre Wachsmuth <sensenmann5@gmail.com>
2025-04-20 12:58:53 +0100

    dark theme fixes

--- packages/lecture/src/utils/monaco.ts
+++ packages/lecture/src/utils/monaco.ts
@@ -1,20 +1,24 @@
 import { useCallback, useEffect, useRef, type MutableRefObject } from "react";
 import type { editor } from "monaco-editor";
 import type { OnMount } from "@monaco-editor/react";
+import { useColorMode } from "@docusaurus/theme-common";
 
 /**
  * Resizes the monaco editor when its container changes size. Do not use the
  * automatic layout option, it will sometimes produce errors due to synchronous
  * resizes.
- * 
+ *
  * The returned onMount function should be set on the monaco editor's onMount
  * property.
  *
  * @param ref The reference on the container you want to measure
  * @returns An onMount function.
  */
-export function useMonacoResize(monacoContainerRef: MutableRefObject<HTMLElement | null>): [OnMount] {
+export function useMonacoResize(
+  monacoContainerRef: MutableRefObject<HTMLElement | null>,
+): [OnMount] {
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+  const { colorMode } = useColorMode();
 
   useEffect(() => {
     if (monacoContainerRef.current) {
@@ -31,10 +35,14 @@ export function useMonacoResize(monacoContainerRef: MutableRefObject<HTMLElement
     return () => {};
   });
 
-  const onMount = useCallback<OnMount>((editor) => {
+  const onMount = useCallback<OnMount>(
+    (editor) => {
       editorRef.current = editor;
       editor.layout();
-  }, []);
+      editor.updateOptions({ theme: colorMode === "dark" ? "vs-dark" : "vs" });
+    },
+    [colorMode],
+  );
 
   return [onMount];
 }
